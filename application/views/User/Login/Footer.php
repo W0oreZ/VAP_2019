c82c3250d8dd27ee810acf3430e2fb25c26f09be
W0oreZ <elkhadiri.abderrazak@gmail.com>
2018-10-10 15:06:34 +0100

    login fix

--- application/views/User/Login/Footer.php
+++ application/views/User/Login/Footer.php
@@ -1,15 +1,15 @@
 
 	    <div id="dropDownSelect1"></div>
 	
-        <script src="<?=base_url('/VAP/public/')?>vendor/jquery/jquery-3.2.1.min.js"></script>
-        <script src="<?=base_url('/VAP/public/')?>vendor/animsition/js/animsition.min.js"></script>
-        <script src="<?=base_url('/VAP/public/')?>vendor/bootstrap/js/popper.js"></script>
-        <script src="<?=base_url('/VAP/public/')?>vendor/bootstrap/js/bootstrap.min.js"></script>
-        <script src="<?=base_url('/VAP/public/')?>vendor/select2/select2.min.js"></script>
-        <script src="<?=base_url('/VAP/public/')?>vendor/daterangepicker/moment.min.js"></script>
-        <script src="<?=base_url('/VAP/public/')?>vendor/daterangepicker/daterangepicker.js"></script>
-        <script src="<?=base_url('/VAP/public/')?>vendor/countdowntime/countdowntime.js"></script>
-        <script src="<?=base_url('/VAP/public/')?>js/main.js"></script>
+        <script src="<?=base_url('/VAP/public/login/')?>vendor/jquery/jquery-3.2.1.min.js"></script>
+        <script src="<?=base_url('/VAP/public/login/')?>vendor/animsition/js/animsition.min.js"></script>
+        <script src="<?=base_url('/VAP/public/login/')?>vendor/bootstrap/js/popper.js"></script>
+        <script src="<?=base_url('/VAP/public/login/')?>vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="<?=base_url('/VAP/public/login/')?>vendor/select2/select2.min.js"></script>
+        <script src="<?=base_url('/VAP/public/login/')?>vendor/daterangepicker/moment.min.js"></script>
+        <script src="<?=base_url('/VAP/public/login/')?>vendor/daterangepicker/daterangepicker.js"></script>
+        <script src="<?=base_url('/VAP/public/login/')?>vendor/countdowntime/countdowntime.js"></script>
+        <script src="<?=base_url('/VAP/public/login/')?>js/main.js"></script>
     
     </body>
     </html>
