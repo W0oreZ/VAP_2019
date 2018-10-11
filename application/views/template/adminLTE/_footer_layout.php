c80b67630765e4ff2f3a223b34a932380de36e48
W0oreZ <elkhadiri.abderrazak@gmail.com>
2018-10-11 15:35:15 +0100

    path fix

--- application/views/template/adminLTE/_footer_layout.php
+++ application/views/template/adminLTE/_footer_layout.php
@@ -1,10 +1,10 @@
 
-<script src="<?=base_url("VAP/public/adminLTE")?>bower_components/jquery/dist/jquery.min.js"></script>
-<script src="<?=base_url("VAP/public/adminLTE")?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="<?=base_url("VAP/public/adminLTE")?>bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script src="<?=base_url("VAP/public/adminLTE")?>bower_components/fastclick/lib/fastclick.js"></script>
-<script src="<?=base_url("VAP/public/adminLTE")?>dist/js/adminlte.min.js"></script>
-<script src="<?=base_url('VAP/public/adminLTE')?>plugins/iCheck/icheck.min.js"></script>
+<script src="<?=base_url("VAP/public/adminLTE/")?>bower_components/jquery/dist/jquery.min.js"></script>
+<script src="<?=base_url("VAP/public/adminLTE/")?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?=base_url("VAP/public/adminLTE/")?>bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="<?=base_url("VAP/public/adminLTE/")?>bower_components/fastclick/lib/fastclick.js"></script>
+<script src="<?=base_url("VAP/public/adminLTE/")?>dist/js/adminlte.min.js"></script>
+<script src="<?=base_url('VAP/public/adminLTE/')?>plugins/iCheck/icheck.min.js"></script>
 <script>
   $(document).ready(function () {
     $('.sidebar-menu').tree();
