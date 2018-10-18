8a73caddd72746bcf84cc9faea56354743b9eefb
W0oreZ <elkhadiri.abderrazak@gmail.com>
2018-10-18 17:52:48 +0100

    display_Annonces

--- application/views/annonce/display_all.php
+++ application/views/annonce/display_all.php
@@ -4,15 +4,16 @@
 					<div id="store">
 						<!-- row -->
 						<div class="row">
-							<!-- Product Single -->
+                            <!-- Product Single -->
+<?php foreach($annonces as $annonce):?>
 							<div class="col-md-4 col-sm-6 col-xs-6">
 								<div class="product product-single">
 									<div class="product-thumb">
 										<button class="main-btn quick-view"><i class="fa fa-search-plus"></i>Afficher les details</button>
-										<img src="./img/product08.jpg" alt="">
+										<img src="<?php echo base_url('/VAP/public/images/annonce/').$annonce['primary_image'] ?>" alt="primaryImage" width='200' height='400'>
 									</div>
 									<div class="product-body">
-										<h3 class="product-price">Rabat</h3>
+										<h3 class="product-price" id="<?php echo $annonce['ville_id'] ?>"><?php echo $annonce['ville_name'] ?></h3>
 										<div class="product-rating">
 											<i class="fa fa-star"></i>
 											<i class="fa fa-star"></i>
@@ -20,15 +21,17 @@
 											<i class="fa fa-star"></i>
 											<i class="fa fa-star-o empty"></i>
 										</div>
-										<h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+										<h2 class="product-name"><a href="#"><?php echo $annonce['titre'] ?></a></h2>
 										<div class="product-btns">
 											<button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
 											<button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-											<button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+											<button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Go to Page</button>
 										</div>
 									</div>
 								</div>
-							</div>
+                            </div>
+                            
+<?php endforeach;?>
 						
 						</div>
 						<!-- /row -->
